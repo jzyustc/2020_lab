82cfa353cf2be067e5003cbc8801d7112779f3c1
dell <1730154258@qq.com>
2020-10-25 18:21:48 +0800

    use test while train to test_ConvNet and test_ResNet_18

--- classifier/example/test_model/test_ConvNet.py
+++ classifier/example/test_model/test_ConvNet.py
@@ -7,8 +7,8 @@ print("device : ", device, '\n')
 
 net = ConvNet(64, device)
 
-net.training_model(10, 1500, plot=True)
+net.training_model(10, 1500, plot=True, timer=True, test=True, test_num=1000)
 
-test_result = net.testing_model()
-print("training loss : " + str(test_result[0]))
-print("training accuracy : " + str(test_result[1]))
+test_result = net.testing_model(timer=True)
+print("testing loss : " + str(test_result[0]))
+print("testing accuracy : " + str(test_result[1]))
--- classifier/example/test_model/test_ResNet_18.py
+++ classifier/example/test_model/test_ResNet_18.py
@@ -5,10 +5,10 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print("device : ", device, '\n')
 
-net = ResNet_18(128, device)
+net = ResNet_18(64, device)
 
-net.training_model(30, 100, plot=True, timer=True)
+net.training_model(30, 1500, plot=True, timer=True, test=True, test_num=1000)
 
 test_result = net.testing_model(timer=True)
-print("training loss : " + str(test_result[0]))
-print("training accuracy : " + str(test_result[1]))
+print("testing loss : " + str(test_result[0]))
+print("testing accuracy : " + str(test_result[1]))
